ec38af5c06e1ba32e98c78b12ff0bced078b12a4
Andrey K <karway211@gmail.com>
2019-10-03 18:57:30 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -35,10 +35,47 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
+
+
 
 function decode(expr) {
     // write your solution here
+    let answer = '';
+    const stringArr = [];
+    const exprArr = [];
+    for (let i = 0; i < expr.length; i+=10) {
+        exprArr.push(expr.slice(i, i + 10));
+    }
+    for (let i = 0; i < exprArr.length; i++) {
+        let string = ''
+        if(exprArr[i] ==='*'){
+            string += '*';
+        }
+        for(let j = 0; j < exprArr[i].length; j+=2){
+            if(exprArr[i][j] + exprArr[i][j+1]==='10'){
+                string += '.';
+            }
+            if(exprArr[i][j] + exprArr[i][j+1]==='11'){
+                string += '-';
+            }
+        }
+        stringArr.push(string);
+    }
+    
+    stringArr.forEach(item => {
+        if(!MORSE_TABLE[item]){
+            answer += ' ';
+        }
+        if(MORSE_TABLE[item]){
+            answer += MORSE_TABLE[item];
+        }
+    });
+
+    return answer;
+
+
 }
 
 module.exports = {
